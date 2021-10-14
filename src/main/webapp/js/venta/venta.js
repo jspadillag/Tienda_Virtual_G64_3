c510d5ac7aeae563b6663d74e67f3ff1caa68eff
Juan Padilla <juanspadillag@gmail.com>
2021-10-14 13:09:31 -0500

    cargue cambios ventas 14 oct

--- src/main/webapp/js/venta/venta.js
+++ src/main/webapp/js/venta/venta.js
@@ -4,17 +4,30 @@ var codigo;
 function mostrar(cedula) {
 	$.ajax({
 		type: "GET",
-		url: "http://localhost:8080/api/usuario/" + cedula,
-		success: function(usuario) {
-			$('#labelUsuario').val(usuario.nombre_usuario);
+		url: "http://localhost:8080/api/cliente/" + cedula,
+		success: function(cliente) {
+			$('#labelUsuario').val(cliente.nombre_cliente);
 		}
 	});
 }
 
-function buscar() {
+/*function buscar() {
 	cedula = document.getElementById("inputCedula").value;
 	mostrar(cedula);
+*/
+
+
+function buscar() {
+	cedula = document.getElementById("inputCedula").value;
+		$.ajax({
+		type: "GET",
+		url: "http://localhost:8080/api/cliente/" + cedula,
+		success: function(cliente) {
+			$('#labelUsuario').val(usuario.nombre_cliente);
+		}
+	});
 }
+
 
 function mostrarProducto(codigo) {
 	$.ajax({
@@ -22,11 +35,33 @@ function mostrarProducto(codigo) {
 		url: "http://localhost:8080/api/producto/" + codigo,
 		success: function(producto) {
 			$('#labelNombre').val(producto.nombre_producto);
+			$('#labelNombre2').val(producto.nombre_producto);
+			$('#labelNombre3').val(producto.nombre_producto);
 		}
 	});
 }
 
-function buscarProducto() {
-	codigo = document.getElementById("inputCodigo").value;
-	mostrarProducto(codigo);
+function buscarProducto(numero) {
+	var codigo;	
+	if(numero == 1){
+		codigo = document.getElementById("inputCodigo").value;
+	}else if (numero = 2) {
+		codigo = document.getElementById("inputCodigoDos").value;
+		}else if (numero = 3) {
+			codigo = document.getElementById("inputCodigoTres").value;
+			}
+	$.ajax({
+		type: "GET",
+		url: "http://localhost:8080/api/producto/" + codigo,
+		success: function(producto) {
+			if(numero == 1) {
+				$('#labelNombre').val(producto.nombre_producto);
+			}else if(numero == 2) {
+				$('#labelNombreDos').val(producto.nombre_producto);
+			}else if(numero == 3) {
+				$('#labelNombreTres').val(producto.nombre_producto);
+				}
+			
+		}
+	});
 }
